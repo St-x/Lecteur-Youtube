f3406af97bf49dd782bbca2f048a417fb69a0a2b
St-x <St-x@neuve.ch>
2018-05-17 01:37:22 +0200

    ChansonCell mise en place

--- Lecteur  Youtube/TableauController.swift
+++ Lecteur  Youtube/TableauController.swift
@@ -14,6 +14,7 @@ class TableauController: UIViewController, UITableViewDelegate, UITableViewDataS
     
     var  chansons = [Chanson]()
     
+    let identifiantCell = "ChansonCell"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,16 +29,41 @@ class TableauController: UIViewController, UITableViewDelegate, UITableViewDataS
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let chanson = chansons[indexPath.row]
+        if let cell = tableView.dequeueReusableCell(withIdentifier: identifiantCell) as? ChansonCell {
+            cell.creerCell(chanson)
+            return cell
+        }
+        
         return UITableViewCell()
+    }
+    
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 170
     }
     
     func ajouterChanson () {
         chansons = [Chanson]()
-        let cool = Chanson(artiste: "Mylene Farmer", titre: "California", code: "JAS2XAvINtc")
-        chansons.append(cool)
-        let bien = Chanson(artiste: "Mylene Farmer", titre: "C'est Une Belle Journee", code: "huZTRymyfpU")
-        chansons.append(bien)
+        let fever = Chanson(artiste: "Mylene Farmer", titre: "California", code: "JAS2XAvINtc")
+        chansons.append(fever)
+        let daylight = Chanson(artiste: "Mylene Farmer", titre: "C'est Une Belle Journee", code: "huZTRymyfpU")
+        chansons.append(daylight)
         
+        let cool = Chanson(artiste: "Casseurs Flowters", titre: "Ils sont cools", code: "_DT-4-jJTZc")
+        chansons.append(cool)
+        let bien  = Chanson(artiste: "Orelsan", titre: "tout va bien", code: "dq6G2YWoRqA")
+        chansons.append(bien)
+        let basique = Chanson(artiste: "Orelsan", titre: "Basique", code: "2bjk26RwjyU")
+        chansons.append(basique)
+        let ronde = Chanson(artiste: "Orelsan", titre: "La terre est ronde", code: "oGdhZyS2ozo")
+        chansons.append(ronde)
+        let san = Chanson(artiste: "Orelsan", titre: "San", code: "PejyoeG_TmA")
+        chansons.append(san)
+        let seul = Chanson(artiste: "Orelsan", titre: "Si seul", code: "cz-PLtXh7Lo")
+        chansons.append(seul)
+        let inacheves = Chanson(artiste: "Casseurs Flowteurs", titre: "Inachevés", code: "MW1eQZ_PEb4")
+        chansons.append(inacheves)
         //ReloadData
         tableView.reloadData()
     }
